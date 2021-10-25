284dcd15dda62484797fcdb05906f885db4b0489
Collin Thornton <collin.thornton@okstate.edu>
2021-10-25 18:55:55 -0500

    updated server

--- Lab4/server.cpp
+++ Lab4/server.cpp
@@ -12,8 +12,12 @@
 #include <netinet/in.h>
 
 #include <chrono>
+#include <iostream>
+#include <thread>
 //#include <wiringSerial.h>
 //#include <wiringPi.h>
+
+#include "joystick.hh"
 
 
 #define PORT 8000
@@ -33,24 +37,118 @@ int main(){
 	//kobuki = serialOpen("/dev/kobuki", 115200);
 
 	//Create connection to client
+
+	Joystick joystick("/dev/input/js0");
+	printf("Joystick found: %d\r\n", joystick.isFound());
+
+
 	createSocket();
 
-	while(true){
-		//Read data from client
-		//readData();
+	JoystickEvent event;
+	unsigned int button;
 
-		auto timePoint = std::chrono::system_clock::now().time_since_epoch();		
-		double seconds = std::chrono::duration_cast<std::chrono::seconds>(timePoint).count();
+	printf("INITIALIZED JOYSTICK \r\n");
 
+	//The joystick creates events when a button or axis changes value.
+	//Sample event from the joystick: joystick.sample(&event)
+
+	//You can interpret these by sampling the events.
+	//Each event has three parameters.
+	//A type, axis or button,
+	//judge if the event is button: event.isButton()
+	//judge if the event is axis: event.isAxis()
+	//A number corresponding to the axis or button pressed: event.number
+	//And a value, Buttons: 0-unpressed, 1-pressed, Axis: -32767 to 0 to 32767: event.value
+
+	float rad = 0;
+	float vel = 0;
+
+	bool shutdown = false;
+	bool useDPAD = true;
+	
+	while(!shutdown){
+		/*Create a series of commands to interpret the
+		joystick input and use that input to move the Kobuki*/
+
+		//Use the following Key Map:
+		//Up     - move the Kobuki forward
+		//Down   - move the Kobuki backward
+		//Left   - rotate the Kobuki 90 degrees counterclockwise
+		//Right  - rotate the Kobuki 90 degrees clockwise
+		//Start  - immediately stop the Kobuki's movement
+		//Select - exit the script and close the Kobuki's connection cleanly		
+		if (joystick.sample(&event))
+		{
+			cout << event << "\t type=" << event.type << " number=" << event.number << " value=" << event.value << endl;
+
+			if (event.isButton())
+			{
+				//printf("isButton: %u | Value: %d\n", event.number, event.value);
+				/*Interpret the joystick input and use that input to move the Kobuki*/
+				switch(event.number) {
+					case 8: // LOGITECH
+						if(event.value != 0) {
+							printf("\nSHUTTING DOWN\n");
+							shutdown = true;
+						}
+						break;
+
+					case 0: 
+						if(event.value != 0) {
+							useDPAD = !useDPAD;
+						}
+						break;
+				}
+
+			}
+			if (event.isAxis())
+			{
+				//printf("isAxis: %u | Value: %d\n", event.number, event.value);
+				/*Interpret the joystick input and use that input to move the Kobuki*/
+
+				switch(event.number) {
+					case 6:	// DPAD left/right
+						if (!useDPAD) break;
+
+						rad = event.value > 0 ? -1 : 1;
+						vel = event.value != 0 ? 3.1415 : 0;
+						break;
+
+					case 7:	// DPAD up/down
+						if (!useDPAD) break;
+
+						rad = 0;
+						if(event.value == 0) vel = 0;
+						else if(event.value > 0) vel = -500;
+						else vel = 500;
+
+						break;
+
+					case 3:
+						if(useDPAD) break;
+						if (event.value == 0) rad = 0;
+						else if(event.value == -32767) rad = -1;
+						else if(event.value == 32767) rad = 1;
+						else if(event.value > 0) rad = (500.0-1.0)/32767.0*event.value - 500.0;
+						else if(event.value < 0) rad = (500.0-1.0)/32767.0*event.value + 500.0;
+						break;
+
+					case 4:
+						if(useDPAD) break;
+
+						vel = -(float)event.value/32767.0 * 500;
+						break;
+				}
+				
+			}
+		}
 		char buffer[1024] = {0};
 
-		sprintf(buffer, "%f", seconds);
-		
-		send(new_socket, buffer, strlen(buffer), 0);
-		//fprintf(new_socket, "test\r\n");
-		sleep(1);
+		sprintf(buffer, "%f:%f:%d:", vel, rad, shutdown);
+		//cout << buffer << "\tUSE DPAD: " << useDPAD << endl;
+		//cout << "isAxis: " << event.isAxis() << "\tnumber: " << event.number << "\tvalue: " << event.value << "\tvel: " << vel << "\trad: " << rad << endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
-	return 0;
 }
 
 void movement(int sp, int r){
